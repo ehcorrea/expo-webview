0de7b4ff35c2734c73c5df46ce8da8a5a30d4d8a
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-29 11:20:22 -0300

    chore: add react-native-webview

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WebView
+        style={{ flex: 1 }}
+        injectedJavaScript=""
+        source={{ uri: 'https://meu.inss.gov.br/#/login' }}
+      />
     </View>
   );
 }
